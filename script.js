7cafdf65a4ae88d8b3b3742f92d39cbee57bc105
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2024-10-07 20:27:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,6 +51,16 @@ const venuscard = document.getElementById("venus-card")
 const venus = document.getElementById("venus-text")
 const earthcard = document.getElementById("earth-card")
 const earth = document.getElementById("earth-text")
+const mars = document.getElementById("mars-text")
+const marscard = document.getElementById("mars-card")
+const jupiter = document.getElementById("jupiter-text")
+const jupitercard = document.getElementById("jupiter-card")
+const saturn = document.getElementById("saturn-text")
+const saturncard = document.getElementById("saturn-card")
+const uranus = document.getElementById("uranus-text")
+const uranuscard = document.getElementById("uranus-card")
+const neptune = document.getElementById("neptune-text")
+const neptunecard = document.getElementById("neptune-card")
 sun.addEventListener('click', () => {
     event.preventDefault();
     suncard.style.visibility = "visible"
@@ -68,11 +78,32 @@ sun.addEventListener('click', () => {
     setTimeout(() => {
         earthcard.style.visibility = "hidden"; // Hide it after animation ends
     }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
 })
 
 universe.addEventListener('click', () => {
     event.preventDefault()
-    universe.style.filter = "none"
+    setTimeout(() => { universe.style.filter = "none" }, 150)
+
     suncard.style.opacity = "0"
     mercurycard.style.opacity = "0"
     setTimeout(() => {
@@ -88,6 +119,26 @@ universe.addEventListener('click', () => {
     earthcard.style.opacity = "0"
     setTimeout(() => {
         earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
     }, 500);
 })
 mercury.addEventListener('click', () => {
@@ -106,6 +157,26 @@ mercury.addEventListener('click', () => {
     earthcard.style.opacity = "0"
     setTimeout(() => {
         earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
     }, 500);
 })
 
@@ -126,12 +197,33 @@ venus.addEventListener('click', () => {
     setTimeout(() => {
         earthcard.style.visibility = "hidden"; // Hide it after animation ends
     }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
 })
 
 earth.addEventListener('click', () => {
     console.log("clicked")
     earthcard.style.visibility = "visible"
-    universe.style.filter = "blur(5px)"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
     earthcard.style.opacity = "1"
     suncard.style.opacity = "0"
     setTimeout(() => {
@@ -145,4 +237,224 @@ earth.addEventListener('click', () => {
     setTimeout(() => {
         venuscard.style.visibility = "hidden"; // Hide it after animation ends
     }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+})
+
+mars.addEventListener('click', () => {
+    console.log("clicked")
+    marscard.style.visibility = "visible"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
+    marscard.style.opacity = "1"
+    suncard.style.opacity = "0"
+    setTimeout(() => {
+        suncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    mercurycard.style.opacity = "0"
+    setTimeout(() => {
+        mercurycard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    venuscard.style.opacity = "0"
+    setTimeout(() => {
+        venuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    earthcard.style.opacity = "0"
+    setTimeout(() => {
+        earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+})
+jupiter.addEventListener('click', () => {
+    console.log("clicked")
+    jupitercard.style.visibility = "visible"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
+    jupitercard.style.opacity = "1"
+    suncard.style.opacity = "0"
+    setTimeout(() => {
+        suncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    mercurycard.style.opacity = "0"
+    setTimeout(() => {
+        mercurycard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    venuscard.style.opacity = "0"
+    setTimeout(() => {
+        venuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    earthcard.style.opacity = "0"
+    setTimeout(() => {
+        earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+})
+saturn.addEventListener('click', () => {
+    console.log("clicked")
+    saturncard.style.visibility = "visible"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
+    saturncard.style.opacity = "1"
+    suncard.style.opacity = "0"
+    setTimeout(() => {
+        suncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    mercurycard.style.opacity = "0"
+    setTimeout(() => {
+        mercurycard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    venuscard.style.opacity = "0"
+    setTimeout(() => {
+        venuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    earthcard.style.opacity = "0"
+    setTimeout(() => {
+        earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+
+})
+
+uranus.addEventListener('click', () => {
+    console.log("clicked")
+    uranuscard.style.visibility = "visible"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
+    uranuscard.style.opacity = "1"
+    suncard.style.opacity = "0"
+    setTimeout(() => {
+        suncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    mercurycard.style.opacity = "0"
+    setTimeout(() => {
+        mercurycard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    venuscard.style.opacity = "0"
+    setTimeout(() => {
+        venuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    earthcard.style.opacity = "0"
+    setTimeout(() => {
+        earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    neptunecard.style.opacity = "0"
+    setTimeout(() => {
+        neptunecard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+
+})
+neptune.addEventListener('click', () => {
+    console.log("clicked")
+    neptunecard.style.visibility = "visible"
+
+    setTimeout(() => { universe.style.filter = "blur(5px)" }, 150)
+    neptunecard.style.opacity = "1"
+    suncard.style.opacity = "0"
+    setTimeout(() => {
+        suncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    mercurycard.style.opacity = "0"
+    setTimeout(() => {
+        mercurycard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    venuscard.style.opacity = "0"
+    setTimeout(() => {
+        venuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    earthcard.style.opacity = "0"
+    setTimeout(() => {
+        earthcard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    marscard.style.opacity = "0"
+    setTimeout(() => {
+        marscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    jupitercard.style.opacity = "0"
+    setTimeout(() => {
+        jupitercard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    saturncard.style.opacity = "0"
+    setTimeout(() => {
+        saturncard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+    uranuscard.style.opacity = "0"
+    setTimeout(() => {
+        uranuscard.style.visibility = "hidden"; // Hide it after animation ends
+    }, 500);
+
 })
